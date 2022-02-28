372954f7f8f41da478e2e3437a95e395b0435aa2
ricardo-qm <ricardo.qm@culiacan.tecnm.mx>
2022-02-28 17:45:49 +0000

    Lista la suma de cuadrados...

---
+++ assignments/00Cuadrados/src/exercise.py
@@ -0,0 +1,14 @@
+def sumaCuadrados(n):
+    suma = 0
+    for i in range(1,n+1):
+        suma += i ** 2
+    return suma
+
+def main():
+    #escribe tu código abajo de esta línea
+    n=int(input("N:"))
+
+    print(f"La suma de cuadrados desde 1 hasta {n:3} es: {sumaCuadrados(n):3}")
+
+if __name__=='__main__':
+    main()
